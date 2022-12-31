8cbadb8854fc5d8ec175eba217b664a28a97fcc4
rayan <rayanzyad99@gmail.com>
2022-12-31 03:50:43 +0000

    Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter the width of the rectangle ");
+        Scanner myinput = new Scanner(System.in);
+        double length = myinput.nextDouble();
+        System.out.println("You entered: " + length);
+    }
+}
